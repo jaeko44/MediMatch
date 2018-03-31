96370a793ac012bdab2bf9bb1aee3f894cf16086
Jonathan Philipos <pavilion.vps@gmail.com>
2018-03-31 21:34:36 +1100

    MedicalProfessional Updated Model

--- Models/MedicalProfessional.cs
+++ Models/MedicalProfessional.cs
@@ -1,40 +1,69 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using System.Threading.Tasks;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.ComponentModel.DataAnnotations;
 
 namespace MediMatchRMIT.Models
 {
     public class MedicalProfessional
     {
+        [Key]
         public int MedicalId { get; set; }
+        public string FacilityName { get; set; }
         public string LastName { get; set; }
         public string FirstMidName { get; set; }
-        public string Address { get; set; }
+        public virtual Address Location { get; set; }
         public virtual HoursActive ServiceActive { get; set; }
-
         public string Notes { get; set; }
-
         public string Website { get; set; }
-
         public string Email { get; set; }
         public string PhoneNumber { get; set; }
-        public List<string> MainCategory { get; set; }
-        public List<string> SubCategory { get; set; }
-
-        public List<string> Facilities { get; set; }
-
-        public Reviews Feedback { get; set; }
+        public virtual ICollection<Service> Services { get; set; }
+        public virtual ICollection<Facility> Facilities { get; set; }
+        public virtual Reviews Feedback { get; set; }
 
     }
-
+    public class Service
+    {
+        [Key]
+        public int ServiceId { get; set; }
+        public string ServiceType { get; set; }
+    }
+    public class Facility
+    {
+        [Key]
+        public int ServiceId { get; set; }
+        public string Support { get; set; }
+    }
+    public class Address
+    {
+        [Key]
+        public int MedicalId { get; set; }
+        public int PostCode { get; set; }
+        public int Street { get; set; }
+        public int StreetNo { get; set; }
+        public string Suburb { get; set; }
+        public virtual DecimalDegrees Coordinates { get; set; }
+    }
+    public class DecimalDegrees
+    {
+        [Key]
+        public decimal Latitude { get; set; }
+        public decimal Longtitude { get; set; }
+    }
     public class HoursActive
     {
-        public List<string> WeekDays { get; set; }
-        public List<string> Hours { get; set; }
+        [Key]
+        public string WeekDays { get; set; }
+        public string Hours { get; set; }
     }
     public class Reviews
     {
+        [Key]
+        public int ReviewId { get; set; }
         public string Title { get; set; }
         public int Rating { get; set; }
         public string Comment { get; set; }
