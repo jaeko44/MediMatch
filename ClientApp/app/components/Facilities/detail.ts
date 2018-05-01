3654c5ce2e9c6d99c09b88d10a17ff750cfaa8eb
Jonathan Philipos <pavilion.vps@gmail.com>
2018-05-02 05:54:22 +1000

    Add google maps integration to facility

--- ClientApp/app/components/Facilities/detail.ts
+++ ClientApp/app/components/Facilities/detail.ts
@@ -6,6 +6,9 @@ export class DetailFacility {
     http: HttpClient;
     public facility: facility;
     public medicalId: number;
+    public googleMapsKey: string = "AIzaSyBkkj6qQ0qLfTGHYJPuL9asFAAk9hlguJ4";
+    public facilityAddress: string;
+    public clientAddress: string;
 
     constructor(http: HttpClient) {
         this.http = http;
@@ -15,10 +18,38 @@ export class DetailFacility {
             .then(result => result.json() as Promise<facility>)
             .then(data => {
                 this.facility = data;
+                this.facilityAddress = this.facility.location.streetNo + "+" +
+                    this.facility.location.street + "," +
+                    this.facility.location.suburb + "+" +
+                    this.facility.location.postCode;
+                this.clientAddress = this.facilityAddress;
+                this.getLocation();
                 console.log(data);
             });
     }
+    getLocation() {
+        if (navigator.geolocation) {
+            navigator.geolocation.getCurrentPosition((position) => {
+                // create the map here, because we only have access to position inside of this function
+                // even if we store in a global variable, it only gets updated once this callback runs
+                this.http.fetch('https://maps.googleapis.com/maps/api/geocode/json?latlng=' + position.coords.latitude + "," + position.coords.longitude + "&key=" + this.googleMapsKey)
+                    .then(result => result.json() as Promise<any>)
+                    .then(data => {
+                        console.log(data);
+                        this.clientAddress = data.results[0].formatted_address;
+                        console.log(this.clientAddress);
+                    });
+            });
+        } else {
+            this.clientAddress = this.facilityAddress;
+        }
+    }
+    showPosition(position, self) {
+        console.log(position);
+        //this.clientAddress = position.coords.latitude + "+" + position.coords.longitude;
 
+        console.log(self.clientAddress);
+    }
     getfeedbackRating() {
 
     }
