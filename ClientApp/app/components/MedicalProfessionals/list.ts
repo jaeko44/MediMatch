8875616a2e10b3eaebdeb039d90fc98e67aa310b
Jonathan Philipos <pavilion.vps@gmail.com>
2018-05-08 22:55:44 +1000

    New Design for List & Back-end changes

--- ClientApp/app/components/MedicalProfessionals/list.ts
+++ ClientApp/app/components/MedicalProfessionals/list.ts
@@ -1,5 +1,6 @@
 import { inject } from 'aurelia-framework';
 import { Data } from '../data';
+import PromptBoxes from 'prompt-boxes';
 
 @inject(Data)
 export class ListMP {
@@ -7,18 +8,25 @@ export class ListMP {
     private services: any;
     private data: Data;
     private filters: any;
+    private PromptBoxes: any;
 
     constructor(data: Data) {
+        var pb = new PromptBoxes({ 
+            toastDir: 'top',        // What position to show the toast (top | bottom)
+            toastMax: 3,            // Max number of toasts to display at once
+            promptAsAbsolute: true  // Whether to show prompt as position absolute (fixes ios input bug)
+          });
+        this.PromptBoxes = pb;
         this.data = data;
         var medicalProfessionalPromise = this.resolveMedicalProfessionals();
         var medicalProfessionalPromise = this.resolveServices();
-
     }
     async resolveMedicalProfessionals() {
         try {
             let data = await this.data.getMedicalProfessionals();
             this.medicalProfessionals = data;
             console.log(this.medicalProfessionals);
+            this.PromptBoxes.success("Succesfully loaded medical professionals");
             return data;
         } catch(error) {
             console.error(error);
@@ -40,8 +48,10 @@ export class ListMP {
             let data = await this.data.filterMedicalProfessionals(this.filters);
             this.medicalProfessionals = data;
             console.log(data);
+            this.PromptBoxes.success("Filtered medical professionals");
             return data;
         } catch(error) {
+            this.PromptBoxes.error("Error filtering medical professionals");
             console.error(error);
             return null;
         }
