219897f501962a96fdccf669d90724289cc4e39e
Jonathan Philipos <pavilion.vps@gmail.com>
2018-05-16 21:11:36 +1000

    Implement Sorting by Location in ClientApp

--- ClientApp/app/components/Facilities/list.ts
+++ ClientApp/app/components/Facilities/list.ts
@@ -5,21 +5,29 @@ import { Common } from '../common';
 @inject(Data, Common)
 export class ListFacilities {
     public Facilities: any;
-    private data: Data;
-    private filters: any;
-    private common: any;
+    public data: Data;
+    public filters: any;
+    public common: any;
+    public location: any;
+    public sort: Sort;
 
-
+ 
     constructor(data: Data, common: Common) {
         this.data = data;
         this.common = common;
         this.resolveFacilities();
+        this.getLocation();
+        this.sort = {
+            property: "facility.facilityName",
+            direction: "ascending"
+        }
     }
     async resolveFacilities() {
         try {
             let data = await this.data.getFacilities();
             this.Facilities = data;
             this.common.notify("GET", "Facilities", "success");
+            this.CaclulateFacilitiesDistance();
             return data;
         } catch (error) {
             this.common.notify("GET", "Facilities", "warning");
@@ -29,10 +37,22 @@ export class ListFacilities {
     }
     async SearchFacilities() {
         try {
-            let data = await this.data.filterFacilities(this.filters);
-            this.Facilities = data;
+            let data = null;
+            if (this.filters.identity != undefined || this.filters.location != undefined || this.filters.any != undefined) {
+                data = await this.data.filterFacilities(this.filters);
+            }
             console.log(data);
-            this.common.notify("FILTER", "Facilities", "success");
+            if (data != null) {
+                this.Facilities = data;
+                this.common.notify("FILTER", "Facilities", "success");
+            }
+            if (this.filters.distance == true) {
+                this.SortFacilitiesByDistance();
+            }
+            else {
+                this.common.notify("FILTER", "Filters missing", "warning");
+            }
+            
             return data;
         } catch (error) {
             this.common.notify("FILTER", "Facilities", "warning");
@@ -40,4 +60,78 @@ export class ListFacilities {
             return null;
         }
     }
+
+    SortFacilitiesByDistance() {
+        try {
+            let data = this.Facilities;
+            this.sort = {
+                property: "facility.distance",
+                direction: "ascending"
+            }
+            console.log(this.sort);
+            this.CaclulateFacilitiesDistance();
+        } catch (error) {
+            this.common.notify("Sort", "Facilities by Location", "warning");
+            console.error(error);
+        }
+
+    }
+
+    CaclulateFacilitiesDistance() {
+        this.Facilities.forEach(facility => {
+            var lat1 = facility.location.coordinates.latitude;
+            var long1 = facility.location.coordinates.longitude
+            var lat2 = self.sessionStorage.getItem("usr_latitude");
+            var long2 = self.sessionStorage.getItem("usr_longitude");
+
+            var distance = this.getDistanceFromLatLonInKm(lat1, long1, lat2, long2);
+            facility.distance = distance;
+            console.log(distance);
+        });
+    }
+
+    getDistanceFromLatLonInKm(lat1, lon1, lat2, lon2) {
+        var R = 6371; // Radius of the earth in km
+        var dLat = this.deg2rad(lat2 - lat1);  // deg2rad below
+        var dLon = this.deg2rad(lon2 - lon1);
+        var a =
+            Math.sin(dLat / 2) * Math.sin(dLat / 2) +
+            Math.cos(this.deg2rad(lat1)) * Math.cos(this.deg2rad(lat2)) *
+            Math.sin(dLon / 2) * Math.sin(dLon / 2)
+            ;
+        var c = 2 * Math.atan2(Math.sqrt(a), Math.sqrt(1 - a));
+        let d = R * c; // Distance in km
+        let dS = d.toFixed(2); // 1.24
+        return dS; 
+    }
+
+    deg2rad(deg) {
+        return deg * (Math.PI / 180)
+    }
+
+    getLocation() {
+        var options = {
+            enableHighAccuracy: true,
+            timeout: 5000,
+            maximumAge: 0
+        };
+        if (navigator.geolocation) {
+            navigator.geolocation.getCurrentPosition(this.positionFound, this.positionError, options);
+        } else {
+            this.location = null;
+        }
+    }
+    positionFound(position) {
+        console.log("Finally found position");
+        self.sessionStorage.setItem("usr_latitude", position.coords.latitude);
+        self.sessionStorage.setItem("usr_longitude", position.coords.longitude);
+    }
+    positionError(error) {
+        console.log(error);
+    }
+}
+
+interface Sort {
+    property: string;
+    direction: string;
 }
